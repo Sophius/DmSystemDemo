05078639081364c7780af67b5699262064541f45
HickeyHsu <alex454141@163.com>
2018-04-24 12:54:09 +0800

    0424更新说明

--- src/application/DmMain.java
+++ src/application/DmMain.java
@@ -5,8 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+//开发与更新记录
+//0424 修复了上传Github时发生的未知Bug -许嘉熙
+//0424 增加了加减分功能按钮 - 许嘉熙
+//0420 增加了随机点名功能 -许嘉熙
+//0420 增加了单独点名面板 -张欣予
+//0419 增加了还原功能，改变了复制文件的方法，修复了还原后无法读取的Bug -许嘉熙
+//0419 增加了缺勤记录功能 -许嘉熙
+//0418 改变了面板打开关闭的方式和时序 -张欣予 许嘉熙
+//0418 增加了教师登录系统注册和登录功能
+//0418 增加了登录界面 张欣予
+//0417 开发出管理面板初版 -许嘉熙
 
-//0424 增加了加减分选项 -许嘉熙
 public class DmMain extends Application {
 	public static int i = 0;
 
